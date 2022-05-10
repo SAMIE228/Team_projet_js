a10b1b03dcf7d447bd3b242e901d3f3180627de0
iMac G1 <imacg1@iMac-de-Energy-13.local>
2022-05-10 13:59:54 +0000

    day10 commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// ajout de l'evenement click au bouton ajouter et lui faire exécuté la fonction d'ajout des taches
+
+document.getElementById("ajouter").addEventListener("click",function (){
+
+    console.log("sa marche");
+})
+
